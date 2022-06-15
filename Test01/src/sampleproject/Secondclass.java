b9836cf90fbe7aa75c6a454b338a21b07f3945c3
madhu <madhu@LAPTOP-DAU977RG>
2022-06-15 09:57:45 +0530

    Update

--- Test01/src/sampleproject/Secondclass.java
+++ Test01/src/sampleproject/Secondclass.java
@@ -3,7 +3,7 @@ package sampleproject;
 public class Secondclass {
 
 	public static void main(String[] args) {
-		System.out.println("Java programmer");
+		System.out.println("Java programmer update");
 
 	}
 
